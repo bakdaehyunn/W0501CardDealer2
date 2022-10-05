04f7ea04fa14a3710ab9b7746569d7bda4092121
home <hennei@naver.com>
2022-10-05 15:51:01 +0900

    1

--- app/src/main/java/kr/ac/kumoh/s20181370/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20181370/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.ac.kumoh.s20181370
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kr.ac.kumoh.s20181370.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +14,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.card1.setImageResource(R.drawable.c_2_of_hearts)
+        getCardName(32)
+
+    }
+    private fun getCardName(c: Int): String{
+        Log.i("getCardName",c.toString())
+        val shape =when(c/13){
+            0->"spades"
+            1->"diamonds"
+            else->"error"
+        }
+        return "c_4_of_spades"
     }
 }
